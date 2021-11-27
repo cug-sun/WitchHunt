dcbd9e81667dda5d6d55bea9389ad1189ad8e36c
Sun <sun.sun@utt.fr>
2021-11-28 00:28:58 +0100

    some tests

--- src/WitchHunt/Main.java
+++ src/WitchHunt/Main.java
@@ -11,7 +11,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
 		Game witchHunt = new Game();
-		witchHunt.initPlayer();
+//		witchHunt.initPlayer();
 //		Player p1 =new Player(1, Identity.Villager);
 //		Player p2 = new Player(2, Identity.Witch);
 //		aPlayer.revealIdentity();
