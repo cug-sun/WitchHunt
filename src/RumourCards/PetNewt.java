c86e44cfd529ee58d5166449cf3765040a2c1cb8
Sun <sun.sun@utt.fr>
2021-12-02 02:12:53 +0100

    fix Pet Newt

--- src/RumourCards/PetNewt.java
+++ src/RumourCards/PetNewt.java
@@ -1,6 +1,7 @@
 package RumourCards;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 import WitchHunt.Game;
@@ -23,6 +24,7 @@ public class PetNewt extends RumourCard {
 	
 	public boolean exsistCards(Game game) {
 		int i = 0;
+		getExsistRevealed = new ArrayList<RumourCard>();
 		for (Player  player : game.getPlayerList()) {
 			if (player.equals(game.getCurrentPlayer())) {
 				continue;
@@ -51,27 +53,42 @@ public class PetNewt extends RumourCard {
 	@Override
 	public void huntEffect(Game game) {
 		// TODO 自动生成的方法存根
-Player player = game.getCurrentPlayer();
+		Player player = game.getCurrentPlayer();
 		
 		//is there any card is revealed by others
 		if (exsistCards(game) == true) {
 			for (RumourCard card : getExsistRevealed) {
+				System.out.println("Take one revealed Rumour card into your hand:");
 				System.out.printf("%d. %s\n",getExsistRevealed.indexOf(card)+1, card.getCardName().toString());
 				System.out.println("Take one of revealed Rumour cards into your hand");
 				Scanner scanner = new Scanner(System.in);
-				RumourCard choosedCard = getExsistRevealed.get(scanner.nextInt() - 1);
-				choosedCard.isUsed = false;
-				System.out.printf("You take %s into your hand\n", choosedCard.getCardName().toString());
-				player.addHand(choosedCard);
+				RumourCard chosenCard = getExsistRevealed.get(scanner.nextInt() - 1);
+				chosenCard.setIsUsed(false);
+				System.out.printf("You take %s into your hand\n", chosenCard.getCardName().toString());
+				player.addHand(chosenCard);
+				//remove chosen card from previous player
+				for(Player p: game.getPlayerList()) {
+					if (!p.getRevealedCards().isEmpty()) {
+						for (Iterator<RumourCard> it = p.getRevealedCards().iterator(); it.hasNext();) {
+							RumourCard rumourCard = it.next();
+							if (rumourCard.equals(chosenCard)) {
+								it.remove();
+							}
+							
+						}
+					}
+					
+				}
+				
+				
 				//choose the next player
 				System.out.println("Choose a player to play next turn");
 				player.chooseNextPlayer(game);
-				String nextIdentity = game.getCurrentPlayer().getIdentity().toString();
-				System.out.printf("The player you choose is a %s",nextIdentity);
 			}
 		super.isUsed = true;
-		} else {
-			System.out.println("Other player doesn't revealed any cards");
+		} 
+		else {
+			System.out.println("No one has any revealed rumour card!");
 			game.setCurrentPlayer(game.getCurrentPlayer());
 		super.isUsed = false;
 		}
